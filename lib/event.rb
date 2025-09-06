af46a511f9be27da6c4718444684aa84a45115cc
Andrew Parker <aparker42@gmail.com>
2025-09-06 11:33:37 -0400

    Parse the ical files to combine them
    It looks like google calendar only takes the
    first calendar in the file. Therefore, I need
    to parse the calendars, extract the events, and recombine
    them into a new calendar.

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,5 @@
+require 'icalendar'
+
 class Event
   attr_reader :ical, :url
 
@@ -7,9 +9,20 @@ class Event
   end
 
   def self.from_events(events)
-    combined_ical = events.reject { |event| event.is_a?(UnfetchableEvent) }.map(&:ical).join("\n")
+    icals = events.reject { |event| event.is_a?(UnfetchableEvent) }.map(&:ical).map { |text| Icalendar::Parser.new(text).parse.first }
+
+    representative_ical = icals.first
+
+    combined_ical = Icalendar::Calendar.new
+    combined_ical.version = representative_ical.version
+    combined_ical.prodid = representative_ical.prodid
+
+    icals.each do |ical|
+      ical.events.each { |event| combined_ical.add_event(event) }
+    end
+
     # For now, we'll use a placeholder URL for the combined event.
     # This might need to be revisited later if a combined event needs a meaningful URL.
-    new(combined_ical, "combined_event_url")
+    new(combined_ical.to_ical, "combined_event_url")
   end
 end
--- test/test_event_factory_test.rb
+++ test/test_event_factory_test.rb
@@ -2,19 +2,46 @@ require 'test_helper'
 require 'event'
 require 'unfetchable_event'
 
+ICAL_EVENT = <<EOD
+BEGIN:VEVENT
+UID:66f70a726c06e93e8b4088af@squarespace.com
+DTSTAMP:20250902T200007Z
+DTSTART:20250905T140000Z
+DTEND:20250905T180000Z
+SUMMARY:Sharpening Edge Tools - 1 spot left!
+GEO:41.916901;-84.028829
+LOCATION:Sam Beauford Woodworking Institute\, 1375 N Main St\, Adrian\, MI\
+ , 49221\, United States
+END:VEVENT
+EOD
+
+ICAL_DATA = <<EOD
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Squarespace Inc/Squarespace 6//v6//EN
+#{ICAL_EVENT}
+END:VCALENDAR
+EOD
+
 class TestEventFactory < Minitest::Test
   def test_concatenates_icals
-    event1 = Event.new("ical1", "url1")
-    event2 = Event.new("ical2", "url2")
+    event1 = Event.new(ICAL_DATA, "url1")
+    event2 = Event.new(ICAL_DATA, "url2")
     combined_event = Event.from_events([event1, event2])
-    assert_equal "ical1\nical2", combined_event.ical
+
+    calendars = Icalendar::Parser.new(combined_event.ical).parse
+    assert_equal 1, calendars.length
+    assert_equal 2, calendars.first.events.length
   end
 
   def test_filters_out_unfetchable_events
-    event1 = Event.new("ical1", "url1")
+    event1 = Event.new(ICAL_DATA, "url1")
     unfetchable_event = UnfetchableEvent.new
-    event2 = Event.new("ical2", "url2")
+    event2 = Event.new(ICAL_DATA, "url2")
     combined_event = Event.from_events([event1, unfetchable_event, event2])
-    assert_equal "ical1\nical2", combined_event.ical
+
+    calendars = Icalendar::Parser.new(combined_event.ical).parse
+    assert_equal 1, calendars.length
+    assert_equal 2, calendars.first.events.length
   end
 end
